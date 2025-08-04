e51cdbe6f63ffaab644da0306ae1a698efa0b6e0
josh <jaaurellano@gbox.ncf.edu.ph>
2025-08-04 13:49:06 +0800

    Added socket io

---
+++ server copy.js
@@ -0,0 +1,58 @@
+const app = require ('./app');
+
+const http = require('http');
+const {WebSocket} = require ('ws');
+const { saveChat, getChat } = require('./controller/chatController');
+
+const server = http.createServer(app);
+
+const PORT = 4000;
+server.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`);
+});
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws,req) => {
+    console.log('New client connected');
+    var id = req.headers['sec-websocket-key']
+    console.log(id)
+    ws.send ('Connected!');
+
+    //Listening message from client
+    ws.on('message',async(chat_details) => {
+        try {
+            const {sender_id, receiver_id, message, type} = JSON.parse(chat_details)
+
+            let data = JSON.parse(chat_details);
+
+            switch (data.type){
+                case('get_message'):
+                const chat_history = await getChat(sender_id, receiver_id)
+                ws.send(JSON.stringify({type: 'CHAT_HISTORY',chat_history}))
+                break;
+
+                case('send_message'):
+                await saveChat(receiver_id, sender_id, message)
+                ws.send(JSON.stringify({ status: 'ok' }));
+
+                wss.clients.forEach((client) => {
+                client.send('Broadcast message!');
+                client.send(JSON.stringify({sender_id, receiver_id, message}))
+                });
+
+                break;
+
+                default:
+                ws.send(JSON.stringify({ type: 'ERROR', message: 'Unknown action type' }));
+            }
+        } catch (error){
+            throw (error)
+        }
+            ws.on('error', (err) => {
+                console.log('Error: ',err);
+            })
+            ws.on('close', () =>{
+            console.log('Client Disconnected')
+            });  
+    })
+});
--- server.js
+++ server.js
@@ -1,58 +1,32 @@
 const app = require ('./app');
 
 const http = require('http');
-const {WebSocket} = require ('ws');
 const { saveChat, getChat } = require('./controller/chatController');
 
 const server = http.createServer(app);
 
+const { Server } = require("socket.io");
+const io = new Server(server, {
+    cors : {
+	origin: (origin, callback) => {
+	  // Allow all origins or validate here
+	  if (origin) {
+		callback(null, origin); // Allow the current origin
+	  } else {
+		callback(null, '*'); // Allow non-browser requests (e.g., Postman)
+	  }
+	},
+	credentials: true, // Allow credentials
+  }
+})
 const PORT = 4000;
 server.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
 });
-const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (ws,req) => {
-    console.log('New client connected');
-    var id = req.headers['sec-websocket-key']
-    console.log(id)
-    ws.send ('Connected!');
-
-    //Listening message from client
-    ws.on('message',async(chat_details) => {
-        try {
-            const {sender_id, receiver_id, message, type} = JSON.parse(chat_details)
-
-            let data = JSON.parse(chat_details);
-
-            switch (data.type){
-                case('get_message'):
-                const chat_history = await getChat(sender_id, receiver_id)
-                ws.send(JSON.stringify({type: 'CHAT_HISTORY',chat_history}))
-                break;
-
-                case('send_message'):
-                await saveChat(receiver_id, sender_id, message)
-                ws.send(JSON.stringify({ status: 'ok' }));
-
-                wss.clients.forEach((client) => {
-                client.send('Broadcast message!');
-                client.send(JSON.stringify({sender_id, receiver_id, message}))
-                });
-
-                break;
-
-                default:
-                ws.send(JSON.stringify({ type: 'ERROR', message: 'Unknown action type' }));
-            }
-        } catch (error){
-            throw (error)
-        }
-            ws.on('error', (err) => {
-                console.log('Error: ',err);
-            })
-            ws.on('close', () =>{
-            console.log('Client Disconnected')
-            });  
-    })
+io.on('connection', (socket) => {
+    console.log(`${socket.id} user just connected`);
+    socket.on('disconnect', () => {
+        console.log('A user disconnect');
+    })   
 });
